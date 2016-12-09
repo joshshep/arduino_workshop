8536aa1f116a8d9c976c63c05dd9df5da96636da
jalien2 <shepherdjosh4@gmail.com>
2016-12-09 12:44:06 -0800

    hhhaanannndzzz

--- simple0/simple0.ino
+++ simple0/simple0.ino
@@ -7,10 +7,10 @@ void setup()
 {
   // everything here gets run once at the beginning
 
-  // set the red pin to output (not input)
+  // set all of the pins for output (not input)
   pinMode(RED_PIN,  OUTPUT);
-  pinMode(GREEN_PIN, OUTPUT);
-  pinMode(BLUE_PIN,OUTPUT);
+  pinMode(GREEN_PIN,OUTPUT);
+  pinMode(BLUE_PIN, OUTPUT);
   pinMode(PIEZO_PIN,OUTPUT);
   
 }
@@ -28,7 +28,7 @@ void loop()
   //turn off the red pin
   digitalWrite(RED_PIN,LOW);
 
-  // turn on the green pin
+  // turn on the green pin, etc
   digitalWrite(GREEN_PIN,HIGH);
   delay(500);
   digitalWrite(GREEN_PIN,LOW);
